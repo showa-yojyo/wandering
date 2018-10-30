534b90a80ab09023dee7c40d306dcff13bf14065
showa_yojyo <yojyo@hotmail.com>
2018-10-30 15:13:02 +0900

    (libraries) Add pages for libraries in 台東区

--- tools/genlib.py
+++ tools/genlib.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 
-KU_PREFIX = 'arakawa'
+KU_PREFIX = 'taito'
 
 LIBRARIES = '''\
-ゆいの森あらかわ               central
-南千住図書館                   minamisenju
-尾久図書館                     ogu
-町屋図書館                     machiya
-日暮里図書館                   nippori
-汐入図書サービスステーション   shioiri
-冠新道図書サービスステーション kammurishindo
+台東区中央図書館	central
+台東区中央図書館浅草橋分室	central-branch-asakusabashi
+台東区中央図書館谷中分室	central-branch-yanaka
+台東区根岸図書館	negishi
+台東区石浜図書館	ishihama
+台東区くらまえオレンジ図書館	kuramae
+台東区すこやかとしょしつ	sukoyaka
+台東区東浅草なかよし図書館	higashiasakusa
 '''
 
 TEMPLATE = '''\
 ---
 title: {title}
+published: false
 ---
 
 TODO: {{{{ page.title }}}}について記述する。
 
-{{% include libraries/{ku}-office-hours.html %}}
+## 休館日
+
+休館日は次のとおり：
+
+* 第 3 木曜日（祝日の場合は翌日振替）
+* 年末年始
+* 特別整理期間
+
+## 営業時間
+
+営業時間は次のとおり：
+
+* 月～土 9:00-20:00
+* 日・祝 9:00-17:00
 '''
 
 def main():
