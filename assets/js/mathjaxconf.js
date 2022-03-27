0a9469cec170f2c8ec2c234d7fa82f7d7dd9c7b6
showa_yojyo <yojyo@hotmail.com>
2022-03-28 01:32:02 +0900

    (assets/mathjaxconf.js) make script modern

--- assets/js/mathjaxconf.js
+++ assets/js/mathjaxconf.js
@@ -80,7 +80,7 @@ window.MathJax = {
 };
 
 (function () {
-    var script = document.createElement('script');
+    const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
     script.async = true;
     document.head.appendChild(script);
