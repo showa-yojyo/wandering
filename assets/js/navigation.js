df4a47a96bf731ab8c170fe518d7cb675152e874
showa_yojyo <2386769+showa-yojyo@users.noreply.github.com>
2024-09-22 00:40:53 +0900

    maintenance [skip ci]

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -31,3 +31,38 @@ document.addEventListener('keydown', event => {
     document.location = link.href;
     event.preventDefault();
 });
+
+// Reference: Vanilla JavaScript Double tap event detection on mobile using
+// setTimeout
+// <https://gist.github.com/ethanny2/44d5ad69970596e96e0b48139b89154b>
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    const pointerTarget = document.body;
+    const DOUBLE_TAP_DURATION = 500;
+    let timeout = 0;
+    let lastTap = new Date().getTime();
+
+    pointerTarget.addEventListener('pointerup', (event) => {
+        const currentTime = new Date().getTime();
+        const tapLength = currentTime - lastTap;
+        lastTap = currentTime;
+
+        if (0 < tapLength && tapLength < DOUBLE_TAP_DURATION) {
+            event.preventDefault();
+            event.target.dispatchEvent(new CustomEvent("doubletap", {
+                bubbles: true,
+                detail: event
+            }));
+        } else {
+            timeout = setTimeout(() => clearTimeout(timeout), DOUBLE_TAP_DURATION);
+        }
+    }, {passive: false});
+
+    pointerTarget.addEventListener('doubletap', (event) => {
+        const rel = event.detail.screenX < screen.width / 2 ? 'prev' : 'next';
+        const link = document.querySelector(`link[rel="${rel}"]`);
+        if (!link){
+            return;
+        }
+        document.location = link.href;
+    });
+}
